dc221fccba6480350c5b69268ce7ad88c9fb0dd5
Hesley97 <hesley.piccoli@hotmail.com>
2023-11-29 11:12:34 -0300

    calculadora

---
+++ rank.js
@@ -0,0 +1,34 @@
+// constantes
+const inicio = "O herói tem saldo de "
+const fim = " e está no nível de"
+
+// função e variavel
+resultado (12 , 6)
+function resultado (vitorias , derrotas){
+	let saldo = (vitorias - derrotas)
+	let nivel
+
+if (vitorias < 10){
+	nivel = " Ferro"
+}
+else if (vitorias >= 11 && vitorias <= 20){
+	nivel = " Bronze"
+}
+else if (vitorias >= 21 && vitorias <= 50){
+	nivel = " Prata"
+}
+else if (vitorias >= 51 && vitorias <= 80){
+	nivel = " Ouro"
+}
+else if (vitorias >= 81 && vitorias <= 90){
+	nivel = " Diamante"
+}
+else if (vitorias >= 91 && vitorias <= 100){
+	nivel = " Lendario"
+}
+else {
+	nivel = " Imortal"
+}
+
+console.log (inicio + saldo + fim + nivel)
+}
